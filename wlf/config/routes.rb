e7e235aab2edfe045cfec3de8cec482795dd4e35
nocotan <noconoco.lib@gmail.com>
2016-08-23 12:15:26 +0900

    Merge

--- wlf/config/routes.rb
+++ wlf/config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'root/index'
   root to: 'root#index'
 
-=======
   resources :exercise_logs
   resources :schedules
   resources :weights
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> master
 end
