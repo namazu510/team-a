dc406807fc8e9614ad3a322b97ac5300dc9e3cf1
naoki <c0114234b7@edu.teu.ac.jp>
2016-08-23 10:46:42 +0900

    modify user migrate

--- wlf/db/migrate/20160823013733_create_users.rb
+++ wlf/db/migrate/20160823013733_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name ,:null=>false
       t.string :email
-      t.boolean :socialId
-      t.boolean :admin
+      t.boolean :socialId ,:null=>false
+      t.boolean :admin ,:null=>false ,default=>false
       t.string :password
       t.string :access_token
       t.string :access_token_secret
