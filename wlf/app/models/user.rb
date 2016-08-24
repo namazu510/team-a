c70c1f697a4ab02d7f824654399e7cd290734b5a
naoki <c0114234b7@edu.teu.ac.jp>
2016-08-24 15:05:01 +0900

    add validates

--- wlf/app/models/user.rb
+++ wlf/app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+
+  validates :gender,  :numericality => {
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+    :less_than => 2
+  }
+
+  validates :height,  :numericality => {
+    :greater_than_or_equal_to => 0
+    :less_than => 300
+  }
+
+  validates :age,  :numericality => {
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+    :less_than => 120
+  }
 end
