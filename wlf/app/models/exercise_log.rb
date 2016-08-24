0f6e2dfc1db68fd1abb512293ab233c75aee202d
naoki <c0114234b7@edu.teu.ac.jp>
2016-08-24 16:48:39 +0900

    add validates

--- wlf/app/models/exercise_log.rb
+++ wlf/app/models/exercise_log.rb
@@ -1,4 +1,16 @@
 class ExerciseLog < ApplicationRecord
   belongs_to :user
   belongs_to :schedule
+
+  validates :step_cnt ,:numericality =>{
+    :only_integer => true
+    :greater_than => 0
+    :less_than => 999999
+  }
+
+  validates :calorie ,:numericality =>{
+    :greater_than => 0
+    :less_than => 999999
+  }
+
 end
